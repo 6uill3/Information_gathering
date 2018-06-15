ff81b5838085f2569ed9924ea8fb0545e6c93abd
6uill3 <6uill3@protonmail.com>
2018-06-15 15:21:38 +0200

    Prueba

--- whois_info.py
+++ whois_info.py
@@ -13,9 +13,42 @@ def whois(source):
     """
     try:
         whois_data = list()
-        answer = pythonwhois.get_whois(source)
-        whois_data.append(answer)
+        short_answer = pythonwhois.get_whois(source)
+        if short_answer.keys()
+        server = ''.join(map(str, short_answer['whois_server']))
+        large_raw_answer = pythonwhois.net.get_whois_raw(source, server=server)
+        parsed_large_raw_answer = pythonwhois.parse.parse_raw_whois(large_raw_answer)
+        whois_data.append(parsed_large_raw_answer)
         if len(whois_data) != 0 and whois_data is not None:
             return whois_data
-    except Exception as e:
+    except pythonwhois.shared.WhoisException as e:
         return None
+
+
+def whois_old(source):
+    """
+
+    :param source:
+    :return:
+    """
+    try:
+        whois_data = list()
+        short_answer = pythonwhois.get_whois(source)
+        whois_data.append(short_answer)
+        if len(whois_data) != 0 and whois_data is not None:
+            return whois_data
+    except pythonwhois.shared.WhoisException as e:
+        return None
+
+
+def get_whois_sever(source):
+    """
+
+    :param source:
+    :return:
+    """
+    try:
+        server = pythonwhois.net.get_root_server(source)
+        print(server)
+    except Exception as e:
+        print('No server')
